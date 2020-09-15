71728e855334fe93adfb4aa73cfdcfdfa073945f
Colin Arel <romans_one16@live.com>
2020-09-15 12:52:32 -0500

    pushing to Git Hub... got weather to react to park select! Woot!

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -10,16 +10,3 @@ export const getWeatherData = () => {
     return fetch(URL)
         .then(response => response.json())
 }
-
-
-// export const getWeatherData = () => {
-//     let headers = new Headers();
-
-//     return fetch(URL, {
-//         method: 'GET',
-//         headers: headers
-//     })
-//         .then(data => {
-//             return data.json();
-//         });
-// }
--- scripts/weather/WeatherSelect.js
+++ scripts/weather/WeatherSelect.js
@@ -1,45 +1,5 @@
 import { getWeatherData } from './WeatherProvider.js';
 
-
-// import { ParkHTML, } from "./Parks.js"
-// import { getParks, useParks } from "./ParkProvider.js"
-
-// // Get a reference to the DOM element where the <select> will be rendered****
-
-// const eventHub = document.querySelector("#getHub")
-// const contentTarget = document.querySelector(".parkHeader")
-
-// export const ParkList = () => {
-//     getParks()
-//         .then(useParks)
-//         .then(render())
-
-// }
-// //Listen for custom event dispatched from ParkSelect
-// eventHub.addEventListener('parkSelected', changeEvent => {
-//     if (changeEvent.detail.parkThatWasSelected !== "0") {
-
-//         // Filter parks app state down to the park that was selected
-
-//         const selectedPark = useParks().filter(currentPark => {
-//             return currentPark.fullName === changeEvent.detail.parkThatWasSelected
-//         })
-//         // Invoke render() and pass the filtered collection as an argument
-//         render(selectedPark)
-//     }
-// })
-
-// /* Map through the array and get the single park ready to be rendered to DOM.
-// Not sure if I need this, because the .filter above should only be returning one park.
-// */
-// const render = (parksCollection = []) => {
-//     contentTarget.innerHTML = parksCollection.map((singlePark) => {
-//         return ParkHTML(singlePark)
-//     }).join("");
-
-// }
-
-
 const CURRENT_LOCATION = document.getElementsByClassName('weather-content__overview')[0];
 const CURRENT_TEMP = document.getElementsByClassName('weather-content__temp')[0];
 const FORECAST = document.getElementsByClassName('component__forecast-box')[0];
