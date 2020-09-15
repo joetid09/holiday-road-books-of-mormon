ecefff74997be56e3a9ad78751ca9031944af9f1
joseph.tidwell09@gmail.com <joseph.tidwell09@gmail.com>
2020-09-15 13:01:55 -0500

    saving before merging with Collin's weather update

--- scripts/eateries/eatery.js
+++ scripts/eateries/eatery.js
@@ -3,19 +3,18 @@ const getHub = document.querySelector("#getHub")
 getHub.addEventListener("change", event => {
 
     if (event.target.id === "eateries") {
-        console.log("did if statement pass?")
         const newEvent = new CustomEvent("eateryChosen", {
             detail: {
                 eateryThatWasChosen: event.target.value
             }
         })
         getHub.dispatchEvent(newEvent)
-        console.log(newEvent.detail.eateryThatWasChosen)
+        // console.log(newEvent.detail.eateryThatWasChosen)
     }
 
 })
 
-getHub.addEventListener("eateryDetails", event => {
+getHub.addEventListener("click", event => {
     if (event.target.id === "eateryDetails") {
         const detailEvent = new CustomEvent("detailsChosen", {
             detail: {
@@ -28,9 +27,26 @@ getHub.addEventListener("eateryDetails", event => {
 })
 
 export const eateryItinerary = (eateryObject) => {
-    return `<h3>${eateryObject.businessName}</h3>
-                <p>${eateryObject.description}</p>
-                <button type="button"id="eateryDetails" onClick="alert('${eateryObject.businessName})"git >details</button>`
+    return `<section class="eateryHead">
+                    <h2>${eateryObject.businessName}</h2>
+                <div class="dropdown">
+                    <span>Detail</span>
+                        <div class="dropdown-content">
+                            <p>${eateryObject.city}, ${eateryObject.state}<p>
+                             <p>${eateryObject.description}</p>
+                         </div>
+                </div>
+            </section>`
+
+    // < h3 > ${ eateryObject.businessName }</h3 >
+    //     <div class="hoverButton">
+    //         <span>Details</span>
+    //         <div class="dropdown-content">
+    //             <p> Located in: ${eateryObject.city}, ${eateryObject.state}</p>
+    //             <p> ${eateryObject.description}</p>
+    //         </div>
+    //     </div>
+
     // <ul class="eateryAmenities" >
     // <li>${eateryObject.amenities.wheelchairAccessible}</li>
     // <li>${eateryObject.}</li>
